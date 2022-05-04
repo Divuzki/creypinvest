048a99c6d303752ddd63b8330d1ea90d34cbc961
Divuzki <divuzki@gmail.com>
2022-05-04 16:14:23 +0100

    Added Contact Us Functionallty

--- core/views.py
+++ core/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render
 
 from creyp.utils import send_contact_us_email
 
+
 def index(request):
     return render(request, "pages/index.html")
 
+
 def about(request):
-    return render(request, "pages/about.html", {"type":"About CreypInvest Inc.", "crumbs":["About Us"]})
+    return render(request, "pages/about.html", {"type": "About CreypInvest Inc.", "crumbs": ["About Us"]})
+
 
 def contact(request):
-    return render(request, "pages/contact.html", {"type":"Contact Support Team At CreypInvest Inc.", "crumbs":["Contact Us"]})
+    return render(request, "pages/contact.html", {"type": "Contact Support Team At CreypInvest Inc.", "crumbs": ["Contact Us"]})
 
 
 def send_contact_email(request):
-    res = render(request, "pages/message_page.html", {"title":"Oops", "msg":"Sorry, something is wrong with the server but you can mail us at <a href='mailto:creypinvest@gmail.com'>creypinvest@gmail.com</a>"})
+    res = render(request, "pages/message_page.html",
+                 {"title": "Oops", "msg": "Sorry, something is wrong with the server but you can mail us at <a href='mailto:creypinvest@gmail.com'>creypinvest@gmail.com</a>"})
     if request.method == "POST":
         name = request.POST.get("name")
         phone = request.POST.get("phone")
@@ -21,9 +25,13 @@ def send_contact_email(request):
         subject = request.POST.get("subject")
         body = request.POST.get("message")
         try:
-            send_contact_us_email(request, name, phone, email, subject, body, toAdmin=True)
-            send_contact_us_email(request, name, phone, email, "Email Recieved!", "Your Email Has Been Received, We Will Get Back To You A Soon As Possible")
-            res = render(request, "pages/message_page.html", {"title":"Yay!", "msg":"Your mail has been sent to us"})
+            send_contact_us_email(request, name, phone,
+                                  email, subject, body, toAdmin=True)
+            send_contact_us_email(request, name, phone, email, "Email Recieved!",
+                                  "Your Email Has Been Received, We Will Get Back To You A Soon As Possible")
+            res = render(request, "pages/message_page.html",
+                         {"title": "Yay!", "msg": "Your mail has been sent to us"})
         except:
-            res = render(request, "pages/message_page.html", {"title":"Oops", "msg":"Sorry, something is wrong with the server but you can mail us at <a href='mailto:creypinvest@gmail.com'>creypinvest@gmail.com</a>"})
+            res = render(request, "pages/message_page.html", {
+                         "title": "Oops", "msg": "Sorry, something is wrong with the server but you can mail us at <a href='mailto:creypinvest@gmail.com'>creypinvest@gmail.com</a>"})
     return res
